f08ec7038262a2d2d10d9f4f2d1f982ce34de501
Kristjan Bujak <cileb411@gmail.com>
2017-11-21 18:15:18 +0100

    prvi komit

---
+++ POP_SF08_2015/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POP_SF08_2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
